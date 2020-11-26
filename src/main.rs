86d76b75fcb8a2cae46b05ebb4c09d39cef4a2e5
stephenyu <stephen@canva.com>
2020-11-27 06:20:46 +1100

    Working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        std::process::exit(0);
+    }
+
+    let target = &args[1].to_lowercase();
+
+    let path = env::current_dir()?;
+    match path.to_str() {
+        Some(path_str) => {
+            let vec: Vec<&str> = path_str.split("/").collect();
+
+            let mut counter = 1;
+            for directory in vec.iter() {
+                if directory.to_lowercase().contains(target) {
+                    let new_path = vec[0..counter].join("/");
+                    println!("{}", new_path);
+                }
+                counter += 1;
+            }
+        }
+        None => println!("Path can't be split"),
+    }
+    Ok(())
 }
